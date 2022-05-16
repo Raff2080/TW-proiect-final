e91c29d2ce2ffd495008c34ada618ddab5074102
Raff2080 <92297111+Raff2080@users.noreply.github.com>
2022-05-16 23:55:51 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+
+function init(){
+
+    document.body.style.backgroundColor=`rgba(${Math.floor(Math.random()*255)%140},${Math.floor(Math.random()*255)%180},${Math.floor(Math.random()*255)%220},${(Math.random()+0.6)/2})`
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+window.onload=init
