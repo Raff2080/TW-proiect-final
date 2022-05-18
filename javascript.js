6ab3568debf8f71e24d53386b44efcfeabd86bcb
Raff2080 <92297111+Raff2080@users.noreply.github.com>
2022-05-18 04:05:25 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,62 @@
 
-
+let logged="no"
 
 function init(){
+
     //background color
     document.body.style.backgroundColor=`rgba(${Math.floor(Math.random()*255)%140},${Math.floor(Math.random()*255)%180},${Math.floor(Math.random()*255)%220},${(Math.random()+0.6)/2})`
-   //display date
+    //logare(mai mult sau mai putin
+    if(!localStorage.getItem("name")){
+        let li=document.createElement("li")
+        let a=document.createElement("a")
+        a.innerHTML="Sign up"
+        a.href="./signUp.html"
+        li.className="menu_li"
+        li.style="float: right"
+        li.append(a)
+        document.body.children[0].children[0].append(li)
+
+
+        let li1=document.createElement("li")
+        let a1=document.createElement("a")
+        a1.innerHTML="Login"
+        a1.href="./login.html"
+        li1.className="menu_li"
+        li1.style="float: right"
+        li1.append(a1)
+        document.body.children[0].children[0].append(li1)
+
+    }
+    else{
+
+        let li1=document.createElement("li")
+        let a1=document.createElement("a")
+        a1.innerHTML="Logout"
+        a1.href="./home.html"
+        li1.className="menu_li"
+        li1.style="float: right"
+        li1.append(a1)
+        document.body.children[0].children[0].append(li1)
+
+        a1.addEventListener("click",(e)=>{
+            localStorage.setItem("name","")
+            localStorage.setItem("email","")
+            localStorage.setItem("religion","")
+        })
+
+        let li=document.createElement("li")
+        let a=document.createElement("a")
+        a.innerHTML=`Welcome, ${localStorage.getItem("name")}`
+        a.href="./profile.html"
+        li.className="menu_li"
+        li.style="float: right"
+        li.append(a)
+        document.body.children[0].children[0].append(li)
+
+
+    }
+
+//display date
     let footer=document.querySelector("#ftr")
     let p=document.createElement("p")
     let today = new Date();
@@ -13,9 +65,50 @@ function init(){
     footer.append(p)
 
 
-        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
-        document.querySelector(".date").textContent=date
+    document.querySelector(".date").textContent=date
+    //coment section
+    let sbm=document.querySelector("#btn")
+    sbm.addEventListener("click",(e)=>{
+        e.preventDefault()
+        let  comm=document.querySelector("#comm").value
+        document.querySelector("#comm").value=""
+        let v=0
+        let k=0
+
+        if(!document.querySelector("#comment").children[0]){
+            localStorage.setItem("nr",0)
+        }
+
+
+        if(!localStorage.getItem("nr")){
+         localStorage.setItem("nr",0)
+        }
+        else{
+
+            k=parseInt(localStorage.getItem("nr"))
+
+        }
+
+        let  div=document.createElement("div")
+        div.innerHTML=comm
+        div.style.width="100%"
+        div.style.height="20px"
+        div.style.border="1px solid black"
+        div.style.backgroundColor="white"
+        div.style.position="absolute"
+        div.style.top=String(k)+"px"
+        div.style.left="0"
+        div.style.overflow="auto"
+        k+=20;
+        localStorage.setItem("nr",k)
+
+        if(comm!=null)
+        document.querySelector("#comment").append(div)
+
+    })
+
 
 
 
---
+++ nodejs.js
@@ -0,0 +1,60 @@
+const express=require("express")
+const ejs=require("ejs")
+const fs=require("fs")
+const expressLayouts= require('express-ejs-layouts')
+const {static} = require("express");
+
+let users=[]
+let k=0
+const server=express()
+
+const data=fs.readFileSync("./data","utf-8")
+
+server.set('view engine', 'ejs')
+
+server.use(express.static('./the_site'))
+server.use(express.urlencoded({extended: false}))
+server.use(expressLayouts)
+
+server.post("/profile.html",(req,res)=>{
+    if(req.body.method==="0"){
+
+        users.push({"name" : req.body.name, "password" : req.body.password, "email" : req.body.email, "religion" : req.body.religion})
+
+        let   replaced=String(data).replace("req.body.name",req.body.name)
+              replaced=replaced.replace("req.body.email",req.body.email)
+              replaced=replaced.replace("req.body.religion",req.body.religion)
+        fs.writeFileSync(__dirname+"/the_site/profile.html",replaced)
+
+
+        res.status(200).send(replaced)
+        res.end()
+    }
+    if(req.body.method==="1") {
+        users.forEach(element => {
+            if (element["name"] === req.body.name && element["password"] === req.body.password && element["email"] === req.body.email) {
+                let   replaced=String(data).replace("req.body.name",req.body.name)
+                replaced=replaced.replace("req.body.email",req.body.email)
+                replaced=replaced.replace("req.body.religion",req.body.religion)
+                fs.writeFileSync(__dirname+"/the_site/profile.html",replaced)
+
+
+                res.status(200).send(replaced)
+                res.end()
+            }
+            else{
+                res.status(200).send(fs.readFileSync(__dirname+"/the_site/login.html","utf-8"))
+                res.end()
+
+            }
+
+        })
+    }
+
+})
+
+
+
+server.listen(5000,(req,res)=>{
+
+})
